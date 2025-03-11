797063661bce8efa379e235ad83b488f4efb2a7d
nth85 <huynt185@gmail.com>
2025-03-11 21:30:56 +0700

    Update test01.sh

--- test01.sh
+++ test01.sh
@@ -7,4 +7,24 @@ A WBS determines if change requests should be approved or rejected.
 50.  she has found an alternate method of performing a task that will save the project and company a great deal of money. What should you, the project manager, do next?
 Evaluate this alternate method and submit a change request. 
 
-51. 
+51. Your supervisor tells you to “beware of the law of diminishing returns,”. What is your supervisor warning you about?
+After a certain point, adding more resources will not yield a proportional increase in productivity.
+
+52.  Which project management method will be most successful for Mathew’s PMO?
+Dynamic programming
+
+53. the project had a future value of $4M with an interest rate of 8% over 2 years. You were able to negotiate the interest rate down to 5%. What is the effect on the present value of your project?
+PV=FV/(1+r)n # mu n=number years, PV=4000.000/(1+0.08) mu 2 = PV=4000.000/(1+0.08)x(1+0.08) =...
+54. 
+55. Which of the following statements about project integration is true?
+Integration is done in all project management process groups. 
+56.  She has determined that her project will have a traditional, predictive life cycle. Additionally, her sponsor and several key stakeholders have provided her with clear, prioritized requirements. What should Kendra do next?
+Create a project scope statement. 
+57. What core competency deals most with mentorship?
+General management
+58. The COO of your software development company has asked you to develop a rough prototype of a piece of software to test a proof of concept for her team. Which development approach is best suited for this project?
+Incremental development
+59. Bruce says that iterative development is better than incremental development when a useable delivery is needed early on for a project. Which of the following is true?
+Incremental delivery yields a useable piece of the project in each iteration. 
+60. 
+

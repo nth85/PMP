9ec02809518b8236cf6e0bcc6a0001916cc79d79
nth85 <huynt185@gmail.com>
2025-03-11 20:38:26 +0700

    Create test01.sh

---
+++ test01.sh
@@ -0,0 +1,10 @@
+48.  Your PMO has asked you to send them a document that outlines how changes will be managed and controlled. What will you send them? 
+Change management plan
+
+49. All of the following statements about a WBS are true except:
+A WBS determines if change requests should be approved or rejected.
+
+50.  she has found an alternate method of performing a task that will save the project and company a great deal of money. What should you, the project manager, do next?
+Evaluate this alternate method and submit a change request. 
+
+51. 
